6a0a7ee65b2e8349d636d49a37dba6e80b49b3aa
Вито <vito@MacBook-Air-Vito.local>
2025-10-29 22:04:20 +0300

    add migrations for exercise last changes

---
+++ supabase/migrations/20251029182827_add_user_with_exercise.sql
@@ -0,0 +1,11 @@
+-- Create exercises table
+CREATE TABLE
+    user_with_exercise_work (
+        user_id bigint NOT NULL REFERENCES user_profiles (telegram_id) ON DELETE CASCADE,
+        exercise_id UUID REFERENCES exercises (id) ON DELETE CASCADE,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT TIMEZONE ('utc'::text, NOW()) NOT NULL,
+        updated_at TIMESTAMP WITH TIME ZONE DEFAULT TIMEZONE ('utc'::text, NOW()) NOT NULL
+    );
+
+-- Create indexes for user_with_exercise_work
+CREATE INDEX idx_user_with_exercise_work_user_id ON user_with_exercise_work (user_id);
